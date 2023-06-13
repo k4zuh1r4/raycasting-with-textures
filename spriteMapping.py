6d948f0682f1cd2fa6723ab18a4447b774877e23
k4zuh1r4 <50957332+k4zuh1r4@users.noreply.github.com>
2023-06-13 08:04:35 +0700

    Update spriteMapping.py

--- spriteMapping.py
+++ spriteMapping.py
@@ -10,9 +10,13 @@ class SpritesMapper:
         self._animatedSpritePath = 'resources/sprites/animated_sprites/'
         loadSprite = self.loadSprite #spriteMapping starts here.
         loadNPC = self.loadNPC 
-        loadSprite(StaticSprite(game,'resources/sprites/static_sprites/candlebra.png',(10.5, 3.5)))
-        loadSprite(AnimatedSprite(game,'resources/sprites/animated_sprites/green_light/0.png',(11.5, 3.5)))
-        loadNPC(NPC(game,'resources/sprites/npc/minotaur/0.png', (2.5,3.5)))
+        loadSprite(StaticSprite(game,'resources/sprites/static_sprites/BARREL_1.png',(4.5, 4)))
+        loadSprite(StaticSprite(game,'resources/sprites/static_sprites/BARREL_1.png',(4.5, 5.5)))
+        loadSprite(AnimatedSprite(game,'resources/sprites/animated_sprites/green_light/0.png',(4.5, 5)))
+        loadNPC(NPC(game,'resources/sprites/npc/minotaur/0.png', (12.5,4)))
+        loadNPC(NPC(game,'resources/sprites/npc/minotaur/0.png', (12.5,3.5)))
+        loadNPC(NPC(game,'resources/sprites/npc/minotaur/0.png', (13,8)))
+        loadNPC(NPC(game,'resources/sprites/npc/minotaur/0.png', (14,10)))
     def spriteUpdate(self):
         for sprite in self._spritesList:
             sprite.update()
